9de0b7d7f7abf8dc3e4ab23faf6768eeaea64fd2
gototheTK <gototkno1@gmail.com>
2021-07-30 11:02:45 +0900

    test

---
+++ target/classes/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO Interest (id,counts, name, founder, founderId, createTime) VALUES
+  (1,0, '기본', 'manager',0,now());
+
+
+
+
---
+++ target/classes/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO Interest VALUES (1,0, now(),'manager',0,'기본');
+
+
+
+
